5675dd796aa5308970b415b80692f26dd673cea1
Serhii Lysenko <51413555+serhiilysenko@users.noreply.github.com>
2021-11-29 16:17:19 +0200

    update markup

--- index search.js
+++ index search.js
@@ -1,25 +1,25 @@
 let b =[];
+
 function a (str, word) {
    
     let array = str.split('');   
     let size = word.length;
     let subarray = []; 
-        for (let i = 0; i <Math.ceil(array.length/size); i++){
-            subarray[i] = array.slice((i*size), (i*size) + size);
-        }
+        
+    for (let i = 0; i <Math.ceil(array.length/size); i++){
+       subarray[i] = array.slice((i*size), (i*size) + size);
+    }
     
     let searchValue = word.split('');
    
     searchValue.forEach(function(item) {
-   		  for(let i=0; i < subarray.length; i++) {
-      	    let j = subarray[i].indexOf(item);
-            if (j>=0) {
+      for(let i = 0; i < subarray.length; i++) {
+      	 let j = subarray[i].indexOf(item);
+             if (j > = 0) {
                 b.push([i,j])
-            }  
-       } 
-     })
-   
-   
+             }  
+          } 
+      })  
 }
 
     a('sdfgskdfgsjkdgf', 'skj');
